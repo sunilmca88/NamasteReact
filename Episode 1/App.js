50e9012061a28677aef7c789cc821cdc274ecde3
Sunil Kumar Yadav <sunilmca88@gmail.com>
2023-12-07 10:02:08 +0530

    Update App.js

--- Episode 1/App.js
+++ Episode 1/App.js
@@ -2,11 +2,11 @@
 
 const heading = React.createElement("h1",{id:'heading'},"Hello World from React!");
 const child = React.createElement('div',{id:'child'},[heading, heading]);
-const parentElem = React.createElement('div',{id:'parent'},child);
+const parent = React.createElement('div',{id:'parent'},child);
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root);
-root.render(parentElem);
+root.render(parent);
 
 
 
